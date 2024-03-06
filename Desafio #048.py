bee3c5cd6818ed324b42ce31672cb8dfa220bc01
Gustavo Henrique <160801791+Gugahl@users.noreply.github.com>
2024-03-06 00:37:51 -0300

    Update Desafio #048.py

--- Desafio #048.py
+++ Desafio #048.py
@@ -1,5 +1,4 @@
 """Desafio #048"""
-from time import sleep
 """
 Faça um programa que calcule a soma entre todos os números
 ímpares que são múltiplos de três e que se encontram no in-
@@ -10,8 +9,14 @@ desafio = ' Desafio #048 '
 print(f'{desafio:=^30}')
 print(' ')
 
+cont = 0
+soma = 0
 for impar in range(1-1, 500+1):
     if impar % 3 == 0:
-        print(impar)
-        sleep(0.5)
+        if impar % 2 != 0:
+            soma += impar
+            cont += 1
+
+print(f'A soma de todos os {cont} valores ímpares, que são múltiplos de três e que se encontram no\n'
+    f'intervalo entre 1 e 500 é igual a: {soma}.')
 print('Fim!')
